71a887d354b29328b946c9b74d8038c9e69bb7e5
binhnd <binhnd.nta@gmail.com>
2021-07-22 08:37:38 +0700

    Initial commit

---
+++ app/src/main/java/com/sevenminuteworkout/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.sevenminuteworkout
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    /**
+     * This method is auto created by Android when the Activity Class is created.
+     */
+    override fun onCreate(savedInstanceState: Bundle?) {
+        //This call the parent constructor
+        super.onCreate(savedInstanceState)
+        // This is used to align the xml view to this class
+        setContentView(R.layout.activity_main)
+
+        // Click event for start Button which we have created in XML.
+        llStart.setOnClickListener {
+            val intent = Intent(this, ExerciseActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
